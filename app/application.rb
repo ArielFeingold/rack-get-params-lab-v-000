2ca6d2b4d70e058b4d73c32742cdb5f5e22ec982
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-26 16:59:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -43,6 +43,7 @@ class Application
   def handle_add(item)
     if @@items.include?(item)
       @@cart << item
+      return "Item added to your cart"
     else
       return "We don't have that item"
     end
